8a24e1e2557fa86de5a61d0a1eeb58e66fb47470
soroushseifpour <66325488+soroushseifpour@users.noreply.github.com>
2020-06-06 07:25:50 -0700

    amade sazi e panel karfaram va view an

--- final/final/App_Start/RouteConfig.cs
+++ final/final/App_Start/RouteConfig.cs
@@ -17,6 +17,7 @@ namespace final
                 name: "Default",
                 url: "{controller}/{action}/{id}",
                 defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+                , namespaces: new[] { "final.Web.Controllers" }
             );
         }
     }
---
+++ final/final/Areas/KarFarma/Controllers/PanelController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace final.Areas.KarFarma.Controllers
+{
+    public class PanelController : Controller
+    {
+        // GET: KarFarma/Panel
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult Jobs()
+        {
+            return View();
+        }
+        public ActionResult JobsForm()
+        {
+            return View();
+        }
+        public ActionResult RecievedResume()
+        {
+            return View();
+        }
+    }
+}
