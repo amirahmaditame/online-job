84773804b201aaccf4c18459a776515157fea2d7
rezajahangiri9876 <67458054+rezajahangiri9876@users.noreply.github.com>
2020-07-27 21:31:21 +0430

    Create AboutUS ans ContactUS Pages

---
+++ final/final/Controllers/PagesController.cs
@@ -0,0 +1,39 @@
+﻿using DataLayer;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+
+namespace final.Controllers
+{
+    public class PagesController : Controller
+    {
+        OnlineJobEntities db = new OnlineJobEntities();
+
+        [Route("About")]
+        public ActionResult About()
+        {
+            return View();
+        }
+        [Route("Contact")]
+        public ActionResult Contact()
+        {
+            return View();
+        }
+
+        [Route("Contact")]
+        [HttpPost]
+        public ActionResult Contact(ContactMessage message)
+        {
+            if (ModelState.IsValid)
+            {
+                db.ContactMessage.Add(message);
+                db.SaveChanges();
+                return View("successText");
+            }
+            return View();
+        }
+    }
+}
